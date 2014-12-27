99bfea850d28988560999c1c96a036fa6b72ad63
Brendan Ribera <brendan.ribera@gmail.com>
2014-12-27 09:08:15 -0800

    Read graph file. Preparing to parse.

--- 107/haskell/107.hs
+++ 107/haskell/107.hs
@@ -1,1 +1,20 @@
--- TODO
+import System.IO
+import Control.Monad
+
+splitBy :: (a -> Bool) -> [a] -> [[a]]
+splitBy _ [] = []
+splitBy f list = first : splitBy f (dropWhile f rest) where
+  (first, rest) = break f list
+
+readNodes c = ((map (map readNode)) .
+               (map (splitBy (== ','))) .
+               lines) c
+  where
+    readInt = read :: String -> Integer
+    readNode s = if s == "-"
+                 then -1
+                 else read s
+
+readGraph fname = do
+  contents <- readFile fname
+  print $ readNodes contents
