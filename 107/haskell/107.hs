73b68611c3773e1a1eb247eac60e149e0c494ee2
Brendan Ribera <brendan.ribera@gmail.com>
2014-12-28 07:50:58 -0800

    Attempting to use `Data.Graph.Inductive.Query.MST`.
    Works for the test case, but the MST returned for the actual problem
    doesn't connect every node!

--- 107/haskell/107.hs
+++ 107/haskell/107.hs
@@ -1,9 +1,13 @@
 import System.IO
 import Control.Monad
 import Data.Char (chr)
-import Data.List (nub)
-import Data.Graph.Inductive.Graph (Graph, Node, LEdge, LNode, mkGraph)
-import Data.Graph.Inductive.Tree (Gr)
+import Data.List (nub, sort, sortBy)
+import Data.Ord (comparing)
+import Data.Graph.Inductive.Graph (Graph, Node, LEdge, LPath(LP), LNode, emap, labEdges, mkGraph)
+import Data.Graph.Inductive.PatriciaTree (Gr)
+import Data.Graph.Inductive.Query.MST (msTree, msTreeAt)
+import Data.Graph.Inductive.Internal.RootPath (getDistance)
+
 
 splitBy :: (a -> Bool) -> [a] -> [[a]]
 splitBy _ [] = []
@@ -13,10 +17,13 @@ splitBy f list = first : splitBy f (dropWhile f rest) where
 withIndexes l = zip [1..] l
 
 readEdges :: String -> [(Node, Node, Int)]
-readEdges c = ((concatMap (\(a, ns) -> map (\(b, w) -> (a, b, w)) ns)) .
+readEdges c = (nub .
+               (concatMap (\(a, ns) -> map (\(b, w) -> if b > a
+                                                       then (a, b, w)
+                                                       else (b, a, w)) ns)) .
                withIndexes .
+               (map (filter (\(_, w) -> w >= 0))) .
                (map (withIndexes)) .
-               (map (filter (>= 0))) .
                (map (map (readNode))) .
                (map (splitBy (== ','))) .
                lines) c
@@ -35,7 +42,20 @@ readGraph c = let edges :: [LEdge Int]
                           (map (\c -> chr (c + 64)) [1..])
               in mkGraph nodes edges
 
+sumWeights gr = (sum . (map (\(_, _, w) -> w)) . labEdges) gr
+
+mstWeights gr = (-- sum .
+                 (sortBy (comparing fst)) .
+                 nub .
+                 (concatMap (\(LP p)-> p)) .
+                 msTree) gr
+
 load fname =
   do
     contents <- readFile fname
-    print $ readGraph contents
+    let graph = readGraph contents
+        totWeight = sumWeights graph
+        minWeight = mstWeights graph
+    print $ totWeight
+    print $ minWeight
+    -- print $ totWeight - minWeight
